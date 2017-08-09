f661bf4922110cd7d7566019bdde5f527259b7ea
TFullerene <s1trojacek@yahoo.com>
2017-08-08 19:28:27 -0500

    Update webcrawler.py

--- webcrawler.py
+++ webcrawler.py
@@ -1,5 +1,7 @@
 import bs4 as bs
 import urllib.request
+import pandas as pd
+
 
 sauce = urllib.request.urlopen('').read()
 soup = bs.BeautifulSoup(sauce, 'lxml')
@@ -8,3 +10,5 @@ soup = bs.BeautifulSoup(sauce, 'lxml')
 
 for paragraph in soup.find_all('p'):
 	print(paragraph.text)
+	
+dfs = pd.read_html()
